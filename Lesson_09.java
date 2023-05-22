c8580534e3f8799d27fe2574cb0d1acc58b17486
symnbe <ookaminingen@me.com>
2023-05-22 08:32:17 +0000

    条件分岐

---
+++ Lesson_09.java
@@ -0,0 +1,53 @@
+//条件分岐
+//if文
+// public class Lesson_09 {
+  
+//   public static void main(String[] args){
+    
+//     int age = 18;
+    
+//     if(age >= 20){
+//       System.out.println("adult");
+//     }
+    
+//   }
+  
+// }
+
+//if else文
+
+// public class Lesson_09 {
+  
+//   public static void main(String[] args){
+    
+//     int age = 22;
+    
+//     if(age >= 20){
+//       System.out.println("adult");
+//     }else {
+//       System.out.println("child");
+//     }
+    
+//   }
+  
+// }
+
+//if else if文
+
+public class Lesson_09 {
+  
+  public static void main(String[] args){
+    
+    int age = 0;
+    
+    if(age >= 20){
+      System.out.println("adult");
+    } else if (age == 0){
+      System.out.println("baby");
+    } else {
+      System.out.println("child");
+    }
+    
+  }
+  
+}

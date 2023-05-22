d28130cbd33d8668f069f2b1cb8ba437ee98e000
symnbe <ookaminingen@me.com>
2023-05-22 06:24:26 +0000

    Youtube_kinoCode_start

---
+++ Lesson_06.java
@@ -0,0 +1,20 @@
+//変数
+public class Lesson_06 {
+
+  public static void main(String[] args) {
+    int var = 0;
+    int VAR = 1;
+    int var01 = 2;
+    int var_01 = 3;
+    int var$01 = 4;
+
+    System.out.println(var);
+    System.out.println(VAR);
+    System.out.println(var01);
+    System.out.println(var_01);
+    System.out.println(var$01);
+  }
+
+}
+//変数名は大文字と小文字の判別がされる
+//変数名には文字、数字（数字から始めることはできない）、_(アンダーバー)、$が使用できる

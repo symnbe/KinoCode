e8692e853d8115063afe03fe468f02cec3f53931
symnbe <ookaminingen@me.com>
2023-05-24 03:43:30 +0000

    繰り返し

--- Lesson_10.java
+++ Lesson_10.java
@@ -1,22 +1,26 @@
-//switchについて
+//繰り返し
 
 public class Lesson_10 {
 
   public static void main(String[] args) {
-
-    String signal = "green";
-    switch(signal){
-      case "red":
-      System.out.println("Stop!!");
-      break;
-      case "yellow":
-      System.out.println("Caoution1");
-      break;
-      case "blue":
-      case "green":
-      System.out.println("Go!!");
-      break;
+    int arr[] = {2, 4, 6, 8, 10};
+    int sum = 0;
+    for(int i = 0; i <= 4; i++){
+      // if(i == 3){
+        // break;
+        // continue;
+      // }
+      
+      // for(int j = 0; j <= 2; j++){
+        
+        // System.out.println(i + "-" + j );
+     // }
+          // System.out.println(arr[i] );
+          sum += arr[i];
+          
+      
     }
+          System.out.println(sum);
   }
 
 }
